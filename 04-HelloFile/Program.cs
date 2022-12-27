1769007c910b7200d6feff49d64021541db94db9
Glenn Poorman <glenn.poorman@autodesk.com>
2022-12-27 12:31:09 -0500

    Modified folder numbers to make room for two additional samples. Added a sample to use very simple static methods on the "File" class to read/write text to/from a disk file.

--- 04-HelloFile/Program.cs
+++ 04-HelloFile/Program.cs
@@ -17,51 +17,28 @@ namespace HelloFile
     {
         static void Main()
         {
-            // The "System.IO.StreamWriter" class is used to write text to a stream using a particular
-            // encoding. In this simplest case, we use it to write text to a a file on disk.
+            // The "System.IO.File" class provides static methods for the creation, copying, deletion,
+            // moving, and opening of a single file.
             //
-            // Create a "StreamWriter" specifying a file in the local folder. Write a line of text to the
-            // file and then close it. If a file by the same name already exists in the folder, it will
-            // be overwritten.
+            // Call the static "File.WriteAllText" method. The first argument is the full  path name of
+            // the file and the second is the text to write to the file. Calling this method creates the
+            // specified file, writes the entirety of the string to the file, and then closes the file.
+            // If the target fle already exists, it is overwritten.
             //
-            // NOTE1: We include a "using" directive for "System.IO" up top so we don't need to fully
-            //        qualify the "StreamWriter" class with the namespace it lives in.
-            //
-            // NOTE2: "Close" is used to close the file and release the handle. In the next section where
-            //        the file is reopened and read, you will see an alternate and preferable way to open
-            //        and close the stream with a variation of the "using" keyword.
+            // NOTE: We include a "using" directive for "System.IO" up top so we don't need to fully
+            //       qualify the "File" class with the namespace it lives in.
             //
             Console.WriteLine("Creating and writing file...");
-            StreamWriter sw = new StreamWriter("HelloFile.txt");
-            sw.WriteLine("Hello World!");
-            sw.Close();
+            File.WriteAllText("HelloFile.txt", "Hello World!");
 
-            // The "System.IO.StreamReader" class is used to read text from a stream. Again we use it
-            // here to read from a file on disk.
-            //
-            // Create a "StreamReader" using the name of the file that was just written. Use a loop to
-            // read lines of text from the file and write them to the console until the end of file is
-            // reached.
-            //
-            // NOTE1: The "ReadLine" method on the reader will return a null reference when the end of
-            //        file is reached.
-            //
-            // NOTE2: An alternative method of closing the file is shown below using another form of the
-            //        "using" keyword. By creating the reader in the context of the "using" statement, we
-            //        assure that the reader is closed when the "using" statement's scope is closed (the
-            //        closing brace). This is applicable to both the reader, writer, and any other type
-            //        that implements the interface "IDisposable" (we will discuss interfaces in a later
-            //        sample).
-            //
-            //        This form is preferable to the explicit "Close" call shown above.
+            // To read the contents of the file back again, call the static "File.ReadAllText" method.
+            // The first an only argument is the full path name of the file (here we use the file we
+            // just created). Calling this method opens the file, reads the entirey of the file into
+            // a single text string, closes the file, and returns that string to the caller.
             //
             Console.WriteLine("\nRe-opening and reading file...");
-            using (StreamReader sr = new StreamReader("HelloFile.txt"))
-            {
-                string strText;
-                while ((strText = sr.ReadLine()) != null)
-                    Console.WriteLine(strText);
-            }
+            string strText = File.ReadAllText("HelloFile.txt");
+            Console.WriteLine(strText);
 
             // Prompt the user to see if the file should be deleted or not.
             //
@@ -71,7 +48,7 @@ namespace HelloFile
             // Use the static string compare method to see how the prompt was answered. The third bool
             // argument specifies that the string compare should be case-insensitive.
             //
-            if (String.Compare(strYesNo, "Y", true) == 0)
+            if (string.Compare(strYesNo, "Y", true) == 0)
             {
                 // Use the static "File.Delete" method to remove the file from the local folder.
                 //
